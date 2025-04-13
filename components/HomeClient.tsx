602fe68aa8223e79c6636ccf600ed34dc3bd822f
mkeloo <mkeloo@ufl.edu>
2025-04-13 00:37:20 -0400

    Fix font variable name and improve formatting in HomeClient component

--- components/HomeClient.tsx
+++ components/HomeClient.tsx
@@ -9,7 +9,10 @@ import Content from "./Content";
 export default function HomeClient() {
 
     return (
-        <div className="w-full flex flex-col min-h-screen bg-cover bg-center  items-center justify-center px-4 md:px-20 lg:px-40 py-10" style={{ backgroundImage: `url(${bgImage.src})` }}>
+        <div
+            className="w-full flex flex-col min-h-screen bg-cover bg-center  items-center justify-center px-4 md:px-20 lg:px-40 py-10"
+            style={{ backgroundImage: `url(${bgImage.src})`, viewTransitionName: "page", }}
+        >
             <div className="w-full h-full flex flex-col items-center lg:items-start justify-center max-w-screen-2xl mx-auto">
 
                 <Card className=" bg-ui-accent/70 w-full max-w-lg rounded-3xl px-3 md:px-6 !gap-4 backdrop-blur-md shadow-lg border border-white/40 text-slate-700">
